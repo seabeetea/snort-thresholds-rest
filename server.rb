49b9fe82b21ed32e1103d3376ab3cef5d4eac70a
seabeetea <seabeetea@users.noreply.github.com>
2015-04-30 10:49:15 -0400

    update for all filters. parameters only set on new and update if they are provided.

--- server.rb
+++ server.rb
@@ -19,7 +19,7 @@ module SnortThresholdsRest
           t.loadfile
           return { 'thresholds' => t.to_a }.to_json
         rescue
-          return internalerror 'there was a problem loading file'
+          return internalerror 'there was a problem loading thresholds'
         end
       end
 
@@ -35,7 +35,7 @@ module SnortThresholdsRest
             { 'valid' => false }.to_json
           end
         rescue
-          return internalerror 'there was a problem validating file'
+          return internalerror 'there was a problem validating thresholds'
         end
       end
 
@@ -47,7 +47,7 @@ module SnortThresholdsRest
           t.loadfile
           return { 'hash' => t.stored_hash }.to_json
         rescue
-          return internalerror 'there was a problem getting file hash'
+          return internalerror 'there was a problem getting thresholds hash'
         end
       end
 
@@ -101,23 +101,23 @@ module SnortThresholdsRest
             return badrequest "invalid filter type #{params[:type]}"
           end
 
-          filter.gid = params['gid'].to_i
-          filter.sid = params['sid'].to_i
-          filter.track_by = params['track_by']
+          filter.gid = params['gid'].to_i if params['gid']
+          filter.sid = params['sid'].to_i if params['sid']
+          filter.track_by = params['track_by'] if params['track_by']
           filter.comment = params['comment'] if params['comment']
-          filter.comment = "##{filter.comment}" if filter.comment and filter.comment[0] != '#'
+          filter.comment = "##{filter.comment}" if filter.comment and filter.comment !~ /\s*#/
 
           if params[:type] == 'suppression'
-            filter.ip = params['ip']
+            filter.ip = params['ip'] if params['ip']
           elsif params[:type] == 'event_filter'
-            filter.type = params['type']
-            filter.count = params['count'].to_i
-            filter.seconds = params['seconds'].to_i
+            filter.type = params['type'] if params['type']
+            filter.count = params['count'].to_i if params['count']
+            filter.seconds = params['seconds'].to_i if params['seconds']
           elsif params[:type] == 'rate_filter'
-            filter.count = params['count'].to_i
-            filter.new_action = params['new_action']
-            filter.seconds = params['seconds'].to_i
-            filter.timeout = params['timeout'].to_i
+            filter.count = params['count'].to_i if params['count']
+            filter.new_action = params['new_action'] if params['new_action']
+            filter.seconds = params['seconds'].to_i if params['seconds']
+            filter.timeout = params['timeout'].to_i if params['timeout']
           end
 
           begin
@@ -164,6 +164,58 @@ module SnortThresholdsRest
           return { 'thresholds' => t_new.to_a }.to_json
         rescue
           return internalerror "there was a problem getting #{params[:type]}"
+        end
+      end
+
+      get '/thresholds/:type/update' do
+        begin
+          content_type :json
+
+          t = Threshold::Thresholds.new
+          t.file = DATAFILE
+          t.loadfile
+
+          filter_type = nil
+          if params[:type] == 'suppression'
+            filter_type = Threshold::Suppression
+          elsif params[:type] == 'event_filter'
+            filter_type = Threshold::EventFilter
+          elsif params[:type] == 'rate_filter'
+            filter_type = Threshold::RateFilter
+          else
+            return badrequest "invalid filter type #{params[:type]}"
+          end
+
+          first_index = t.index.find_index do |filter|
+            filter.is_a?(filter_type) and filter.sid == params['sid'].to_i and filter.gid == params['gid'].to_i
+          end
+
+          if not first_index
+            return notfound "#{params[:type]} not found"
+          end
+
+          t[first_index].track_by = params['track_by'] if params['track_by']
+          t[first_index].comment = params['comment'] if params['comment']
+          t[first_index].comment = "##{t[first_index].comment}" if t[first_index].comment and t[first_index].comment !~ /^\s*#/
+
+          if params[:type] == 'suppression'
+            t[first_index].ip = params['ip'] if params['ip']
+          elsif params[:type] == 'event_filter'
+            t[first_index].type = params['type'] if params['type']
+            t[first_index].count = params['count'].to_i if params['count']
+            t[first_index].seconds = params['seconds'].to_i if params['seconds']
+          elsif params[:type] == 'rate_filter'
+            t[first_index].count = params['count'].to_i if params['count']
+            t[first_index].new_action = params['new_action'] if params['new_action']
+            t[first_index].seconds = params['seconds'].to_i if params['seconds']
+            t[first_index].timeout = params['timeout'].to_i if params['timeout']
+          end
+
+          t.flush
+
+          return { 'thresholds' => t.to_a }.to_json
+        rescue
+          return internalerror "there was a problem updating #{params[:type]}"
         end
       end
 
