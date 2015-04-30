19e6db384be2e6a4279590d52bb97cd7b27ef797
seabeetea <seabeetea@users.noreply.github.com>
2015-04-30 09:25:53 -0400

    generic create, read, and delete for all filters

--- server.rb
+++ server.rb
@@ -11,7 +11,7 @@ module SnortThresholdsRest
         redirect '/ping'
       end
 
-      get '/tresholds' do
+      get '/thresholds' do
         begin
           content_type :json
           t = Threshold::Thresholds.new
@@ -51,112 +51,20 @@ module SnortThresholdsRest
         end
       end
 
-      # post '/suppression/new' do
-      get '/thresholds/suppression/new' do
+      get /\/thresholds\/(suppressions|event_filters|rate_filters)(?:\/(.*))?/ do
         begin
           content_type :json
-
-          s = Threshold::Suppression.new
-          s.gid = params['gid'].to_i
-          s.sid = params['sid'].to_i
-          s.track_by = params['track_by']
-          s.ip = params['ip']
-          s.comment = params['comment'] if params['comment']
-          s.comment = "##{s.comment}" if s.comment and s.comment[0] != '#'
-
-          begin
-            s.to_s
-          rescue
-            return badrequest "invalid suppression"
-          end
-
-          t = Threshold::Thresholds.new
-          t.file = DATAFILE
-          t.loadfile
-          t << s
-          t.flush
-
-          return { 'thresholds' => t.to_a }.to_json
-        rescue
-          return internalerror 'there was a problem creating suppression'
-        end
-      end
-
-      #post '/thresholds/event_filter/new' do
-      get '/thresholds/event_filter/new' do
-        begin
-          content_type :json
-
-          e = Threshold::EventFilter.new
-          e.gid = params['gid'].to_i
-          e.sid = params['sid'].to_i
-          e.type = params['type']
-          e.track_by = params['track_by']
-          e.count = params['count'].to_i
-          e.seconds = params['seconds'].to_i
-          e.comment = params['comment'] if params['comment']
-          e.comment = "##{e.comment}" if e.comment and e.comment[0] != '#'
-
-          begin
-            e.to_s
-          rescue
-            return badrequest "invalid event filter"
-          end
-
-          t = Threshold::Thresholds.new
-          t.file = DATAFILE
-          t.loadfile
-          t << e
-          t.flush
-
-          return { 'thresholds' => t.to_a }.to_json
-        rescue
-          return internalerror 'there was a problem creating event filter'
-        end
-      end
-
-      #post '/thresholds/rate_filter/new' do
-      get '/thresholds/rate_filter/new' do
-        begin
-          content_type :json
-
-          r = Threshold::RateFilter.new
-          r.gid = params['gid'].to_i
-          r.sid = params['sid'].to_i
-          r.track_by = params['track_by']
-          r.count = params['count'].to_i
-          r.new_action = params['new_action']
-          r.seconds = params['seconds'].to_i
-          r.timeout = params['timeout'].to_i
-          r.comment = params['comment'] if params['comment']
-          r.comment = "##{r.comment}" if r.comment and r.comment[0] != '#'
-
-          begin
-            r.to_s
-          rescue
-            return badrequest "invalid rate filter"
-          end
-
-          t = Threshold::Thresholds.new
-          t.file = DATAFILE
-          t.loadfile
-          t << r
-          t.flush
-
-          return { 'thresholds' => t.to_a }.to_json
-        rescue
-          return internalerror 'there was a problem creating rate filter'
-        end
-      end
-
-      get '/thresholds/*' do
-        begin
-          content_type :json
-          known_actions = ['sort', 'reverse', 'shuffle', 'uniq', 'suppressions', 'event_filters', 'rate_filters']
+          known_types = ['suppressions', 'event_filters', 'rate_filters']
+          known_actions = ['sort', 'reverse', 'shuffle', 'uniq']
           actions = []
-          params['splat'].each do |p|
-            p.split('/').each do |a|
-              actions << a
+
+          if not known_types.include?(params['captures'][0])
+            return badrequest "invalid filter type #{params['captures'][0]}"
+          end
+
+          if params['captures'][1]
+            params['captures'][1].split('/').each do |a|
+                actions << a
             end
           end
 
@@ -165,16 +73,128 @@ module SnortThresholdsRest
             t.file = DATAFILE
             t.loadfile
 
-            t_mod = t
+            t_mod = t.send(params['captures'][0])
             actions.each do |a|
               t_mod = t_mod.send(a)
             end
             return { 'thresholds' => t_mod.to_a }.to_json
           else
-            return badrequest "unknown actions: #{(actions - known_actions).join(',')}"
+            return badrequest "invalid actions: #{(actions - known_actions).join(', ')}"
           end
         rescue
           return internalerror "there was a problem with running actions on thresholds"
+        end
+      end
+
+      get '/thresholds/:type/new' do
+        begin
+          content_type :json
+
+          filter = nil
+          if params[:type] == 'suppression'
+            filter = Threshold::Suppression.new
+          elsif params[:type] == 'event_filter'
+            filter = Threshold::EventFilter.new
+          elsif params[:type] == 'rate_filter'
+            filter = Threshold::RateFilter.new
+          else
+            return badrequest "invalid filter type #{params[:type]}"
+          end
+
+          filter.gid = params['gid'].to_i
+          filter.sid = params['sid'].to_i
+          filter.track_by = params['track_by']
+          filter.comment = params['comment'] if params['comment']
+          filter.comment = "##{filter.comment}" if filter.comment and filter.comment[0] != '#'
+
+          if params[:type] == 'suppression'
+            filter.ip = params['ip']
+          elsif params[:type] == 'event_filter'
+            filter.type = params['type']
+            filter.count = params['count'].to_i
+            filter.seconds = params['seconds'].to_i
+          elsif params[:type] == 'rate_filter'
+            filter.count = params['count'].to_i
+            filter.new_action = params['new_action']
+            filter.seconds = params['seconds'].to_i
+            filter.timeout = params['timeout'].to_i
+          end
+
+          begin
+            filter.to_s
+          rescue
+            return badrequest "invalid #{params[:type]}"
+          end
+
+          t = Threshold::Thresholds.new
+          t.file = DATAFILE
+          t.loadfile
+          t << filter
+          t.flush
+
+          return { 'thresholds' => t.to_a }.to_json
+        rescue
+          return internalerror "there was a problem creating #{params[:type]}"
+        end
+      end
+
+      get '/thresholds/:type' do
+        begin
+          content_type :json
+
+          t = Threshold::Thresholds.new
+          t.file = DATAFILE
+          t.loadfile
+
+          filter_type = nil
+          if params[:type] == 'suppression'
+            filter_type = Threshold::Suppression
+          elsif params[:type] == 'event_filter'
+            filter_type = Threshold::EventFilter
+          elsif params[:type] == 'rate_filter'
+            filter_type = Threshold::RateFilter
+          else
+            return badrequest "invalid filter type #{params[:type]}"
+          end
+
+          t_new = t.select do |filter|
+            filter.is_a?(filter_type) and filter.sid == params['sid'].to_i and filter.gid == params['gid'].to_i
+          end
+
+          return { 'thresholds' => t_new.to_a }.to_json
+        rescue
+          return internalerror "there was a problem getting #{params[:type]}"
+        end
+      end
+
+      get '/thresholds/:type/delete' do
+        begin
+          content_type :json
+
+          t = Threshold::Thresholds.new
+          t.file = DATAFILE
+          t.loadfile
+
+          filter_type = nil
+          if params[:type] == 'suppression'
+            filter_type = Threshold::Suppression
+          elsif params[:type] == 'event_filter'
+            filter_type = Threshold::EventFilter
+          elsif params[:type] == 'rate_filter'
+            filter_type = Threshold::RateFilter
+          else
+            return badrequest "invalid filter type #{params[:type]}"
+          end
+
+          t.reject! do |filter|
+            filter.is_a?(filter_type) and filter.sid == params['sid'].to_i and filter.gid == params['gid'].to_i
+          end
+
+          t.flush
+
+          return { 'thresholds' => t.to_a }.to_json
+        rescue
+          return internalerror "there was a problem deleting #{params[:type]}"
         end
       end
 
