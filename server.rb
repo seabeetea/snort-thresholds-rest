160d7b2c6cdc0d3530877e23e3069f2a4cfb03b6
seabeetea <seabeetea@users.noreply.github.com>
2015-04-29 20:41:52 -0400

    creation methods for filters

--- server.rb
+++ server.rb
@@ -7,21 +7,23 @@ module SnortThresholdsRest
 
     class Server < PingableServer
 
-      ['/file/load', '/thresholds'].each do |path|
-        get path do
-          begin
-            content_type :json
-            t = Threshold::Thresholds.new
-            t.file = DATAFILE
-            t.loadfile
-            return { 'thresholds' => t.to_a }.to_json
-          rescue
-            return internalerror 'there was a problem loading file'
-          end
+      get '/' do
+        redirect '/ping'
+      end
+
+      get '/tresholds' do
+        begin
+          content_type :json
+          t = Threshold::Thresholds.new
+          t.file = DATAFILE
+          t.loadfile
+          return { 'thresholds' => t.to_a }.to_json
+        rescue
+          return internalerror 'there was a problem loading file'
         end
       end
 
-      get '/file/valid' do
+      get '/thresholds/valid' do
         begin
           content_type :json
           t = Threshold::Thresholds.new
@@ -37,7 +39,7 @@ module SnortThresholdsRest
         end
       end
 
-      get '/file/hash' do
+      get '/thresholds/hash' do
         begin
           content_type :json
           t = Threshold::Thresholds.new
@@ -50,28 +52,100 @@ module SnortThresholdsRest
       end
 
       # post '/suppression/new' do
-      get '/suppression/new' do
+      get '/thresholds/suppression/new' do
         begin
           content_type :json
 
-          if not params['gid'] or not params['sid']
-            return badrequest 'must provide gid and sid'
+          s = Threshold::Suppression.new
+          s.gid = params['gid'].to_i
+          s.sid = params['sid'].to_i
+          s.track_by = params['track_by']
+          s.ip = params['ip']
+          s.comment = params['comment'] if params['comment']
+          s.comment = "##{s.comment}" if s.comment and s.comment[0] != '#'
+
+          begin
+            s.to_s
+          rescue
+            return badrequest "invalid suppression"
           end
 
           t = Threshold::Thresholds.new
           t.file = DATAFILE
           t.loadfile
-
-          s = Threshold::Suppression.new
-          s.gid = params['gid'].to_i
-          s.sid = params['sid'].to_i
-
           t << s
           t.flush
 
           return { 'thresholds' => t.to_a }.to_json
         rescue
           return internalerror 'there was a problem creating suppression'
+        end
+      end
+
+      #post '/thresholds/event_filter/new' do
+      get '/thresholds/event_filter/new' do
+        begin
+          content_type :json
+
+          e = Threshold::EventFilter.new
+          e.gid = params['gid'].to_i
+          e.sid = params['sid'].to_i
+          e.type = params['type']
+          e.track_by = params['track_by']
+          e.count = params['count'].to_i
+          e.seconds = params['seconds'].to_i
+          e.comment = params['comment'] if params['comment']
+          e.comment = "##{e.comment}" if e.comment and e.comment[0] != '#'
+
+          begin
+            e.to_s
+          rescue
+            return badrequest "invalid event filter"
+          end
+
+          t = Threshold::Thresholds.new
+          t.file = DATAFILE
+          t.loadfile
+          t << e
+          t.flush
+
+          return { 'thresholds' => t.to_a }.to_json
+        rescue
+          return internalerror 'there was a problem creating event filter'
+        end
+      end
+
+      #post '/thresholds/rate_filter/new' do
+      get '/thresholds/rate_filter/new' do
+        begin
+          content_type :json
+
+          r = Threshold::RateFilter.new
+          r.gid = params['gid'].to_i
+          r.sid = params['sid'].to_i
+          r.track_by = params['track_by']
+          r.count = params['count'].to_i
+          r.new_action = params['new_action']
+          r.seconds = params['seconds'].to_i
+          r.timeout = params['timeout'].to_i
+          r.comment = params['comment'] if params['comment']
+          r.comment = "##{r.comment}" if r.comment and r.comment[0] != '#'
+
+          begin
+            r.to_s
+          rescue
+            return badrequest "invalid rate filter"
+          end
+
+          t = Threshold::Thresholds.new
+          t.file = DATAFILE
+          t.loadfile
+          t << r
+          t.flush
+
+          return { 'thresholds' => t.to_a }.to_json
+        rescue
+          return internalerror 'there was a problem creating rate filter'
         end
       end
 
